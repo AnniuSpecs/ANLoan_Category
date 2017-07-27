b18f032e73eaee7ebbe1d330e2cb4a494d645809
daze mac <srjapple2012@gmail.com>
2017-07-27 17:04:35 +0800

    版本号为0.0.2

--- ANLoan_Category.podspec
+++ ANLoan_Category.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "ANLoan_Category"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "ANLoan_Category."
 
   # This description is used to generate tags and improve search results.
--- ANLoan_Category/ANLoan_Category/CTMediator+Loan.m
+++ ANLoan_Category/ANLoan_Category/CTMediator+Loan.m
@@ -11,6 +11,9 @@
 @implementation CTMediator (Loan)
 - (UIViewController *)loan_viewcontroller:(NSDictionary *)params;
 {
+    /*
+     贷款模块
+     */
     return [self performTarget:@"Loan" action:@"viewcontroller" params:nil shouldCacheTarget:NO];
 }
 @end
